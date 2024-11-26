094c85942b8d742d2cdbdd4e3d6cfd89f93c6135
DostonbekZaripov <dastandev0110@gmail.com>
2024-11-27 01:48:15 +0500

    The end

---
+++ main.js
@@ -0,0 +1,97 @@
+// 1-rasm 1-mashq
+// let a = prompt("Son kiriting");
+// if (a>=50 && a<=100){
+//     console.log("Son 50 va 100 oraligida")
+// }
+// else{
+//     console.log("Son 50 va 100 oraligida emas")
+// }
+
+// 1-rasm 2-mashq
+// let son = prompt("Son kiriting");
+// let katta = Math.floor(son);
+// let kichik = Math.ceil(son);
+// console.log(katta);
+// console.log(kichik);
+
+// 1-rasm 3-mashq
+// let a;
+// a= Math.round(Math.random()*100);
+// if(a%2==0){
+//     console.log(`${a} juft `)
+// }
+// else{
+//     console.log(`${a} toq`)
+// }
+
+// 1-rasm 4-mashq
+// let num1 = 13;
+// let num2 = 55;
+// let num3 = 28;
+// let num4 = 25;
+// let result = Math.max(num1,num2,num3,num4);
+// console.log(result)
+
+// 1-rasm 5-mashq
+// let a = 7;
+// let b = 1;
+// if(a=>b){
+//     console.log(a * b)
+// }else{
+//     console.log(a/b)
+// }
+
+// 1-rasm 6-mashq
+// let son = 16; 
+// let ildiz = Math.sqrt(son);
+
+// if (Number.isInteger(ildiz)) {
+//     console.log("Butun ildiz");
+// } else {
+//     console.log("Butun emas");
+// }
+
+// let radius = 7;
+// let diametr = 2 * radius;
+// let uzunlik = 2 * Math.PI * radius;
+// console.log("Radius: " + radius);
+// console.log("Diametr: " + diametr);
+// console.log("Uzunlik (perimetr): " + Math.round(uzunlik));
+
+
+// 2-rasm 2-mashq
+// let a;
+// let b;
+
+// a = Math.round(Math.random()*100);
+// b = Math.round(Math.random()*100);
+
+// if (a>b){
+//     console.log(`${a} yutdi`)
+//     console.log(b)
+// }else{
+//     console.log(a)
+//     console.log(`${b} yutdi`)
+// }
+
+// 3-rasm 1-mashq
+// let number = 5;
+// if (number%1 ===0){
+//     if(number%5===0){
+//         let result = number *10;
+//         console.log(result)
+//     }else{
+//         console.log("Son 5 ga bolinmaydi")
+//     }
+// }else{
+//     console.log("Kiritilgan son butun emas")
+// }
+
+// 4-rasm 1-mashq
+// let a = 5;
+
+// if(a%2==0){
+//     console.log(Math.pow(a,2))
+// }else{
+//     console.log(Math.round(a/3));
+// }
